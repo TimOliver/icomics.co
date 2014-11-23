03e5262d011dd4181c77bb1273c17dfeb5bc14d3
Tim Oliver <me@timoliver.com.au>
2014-11-24 00:31:46 +0800

    Completed the v1.1 redesign implementation.

--- strings.include.php
+++ strings.include.php
@@ -1,3 +1,4 @@
+
 <?php
 /*
 * iComics.co Localisation Strings
@@ -12,27 +13,15 @@ $localisedStrings['en']['langPageDescription'] = 'Supporting all of the major DR
 $localisedStrings['en']['langPageKeywords'] = 'iComics, comic, book, reader, iOS, iPhone, iPod, iPad, CBZ, CBR, PDF, EPUB, CBT, TAR, LZH, LHA, CB7, 7ZIP';
 $localisedStrings['en']['langPageTitle'] = 'iComics - The comic reader for iOS';
 
-$localisedStrings['ja']['langPageDescription'] = 'すぺてのデジタル著作権管理なしコミックのフォーマットを対応していて、iComicsはiPad、iPhone、iPod touchでコミックを読めるアプリです。';
-$localisedStrings['ja']['langPageKeywords'] = 'iComics, コミック, 本, リーダー, iOS, iPhone, iPod, iPad, CBZ, CBR, PDF, EPUB, CBT, TAR, LZH, LHA, CB7, 7ZIP';
-$localisedStrings['ja']['langPageTitle'] = 'iComics - iOSのコミックリーダー';
-
 /* Intro Text */
-$localisedStrings['en']['langIntroText'] = 'iComics is an app that lets you read digital comic books on your iOS devices.<br/>Unlike the other comic apps on the App Store that let you buy comics
-								            from their own collection, iComics lets you bring your own to the table. With many DRM-free comics available online, including indie comic artists distributing their comic books online, 
-								            as well as the vast number of old-school comics that have now become public domain, iComics lets you tap into a much larger world of comic books, all at your fingertips!';
-
-$localisedStrings['ja']['langIntroText'] = 'iComicsはiOSデバイスで著作権管理なしのコミックを読めるアプリです。App Storeでコミックを売っているコミックアプリとは違って、iComicsは自分のコレクションをデバイスに乗せて、読めるアプリです。インタネットでは
-											芸術者が配信している同人コミックやすでに絶版されたコッミクやパッブリックドメインに入ったコミックはものすごくいっぱいありますから、iComicsでコッミクをもっと楽しめます！';
-
+$localisedStrings['en']['langIntroText'] = 'iComics&trade; is an app that lets you read digital comic books on your iOS devices.<br/>Unlike other comic apps on the App Store that act like a store, letting you buy specific comics
+								            from their database, iComics lets you bring your own collection to the table. With the many DRM-free comics now available online, including indie comic artists, 
+								            as well as the vast number of old-school comics that have now become public domain, iComics lets you tap into a much larger world of comic books, directly at your fingertips!';
 /* Comics description text */
-$localisedStrings['en']['langComicsTypeDescription'] = 'iComics supports all of the main DRM-free comic book formats. All you need to do is import your comic book files into iComics 
+$localisedStrings['en']['langComicsTypeDescription'] = 'iComics supports all of the major DRM-free comic book formats. All you need to do is import your comic book files into iComics 
 														via iTunes and they\'ll automatically start unpacking themselves, ready to read in seconds!';
 $localisedStrings['en']['langFormats'] = 'Formats';
 $localisedStrings['en']['langDownloads'] = 'Comic Downloads';
-
-$localisedStrings['ja']['langComicsTypeDescription'] = 'iComicsはすぺてのデジタル著作権管理なしコミックのフォーマットを対応しています。iOSデバイスで読むために、iTunesからコピーする事です。iComicsは自動的に読み込んで、何秒でアクセスされます！';
-$localisedStrings['ja']['langFormats'] = 'フォーマット';
-$localisedStrings['ja']['langDownloads'] = 'ダウンロードサイト';
 
 /* Reading Toggles */
 $localisedStrings['en']['langReadingTogglesDescription'] = 'Comics can come in any shape or size. iComics comes with a suite of display settings to make sure you can read your comics the way they were meant to be read!';
@@ -41,37 +30,18 @@ $localisedStrings['en']['langToggleZoomLock'] = 'Lock zoom scale between pages';
 $localisedStrings['en']['langToggleSplitWidePages'] = 'Split wide pages across screen';
 $localisedStrings['en']['langTogglePageJump'] = 'Jump instantly between pages';
 
-$localisedStrings['ja']['langReadingTogglesDescription'] = 'コミックのページがどんなサイズや形で来られます。画面でちゃんと表示させるために、iComicsは設定たっぷりです！';
-$localisedStrings['ja']['langTogglePageDirection'] = 'ページ方向変更（右か左)';
-$localisedStrings['ja']['langToggleZoomLock'] = 'ページのズームを保持';
-$localisedStrings['ja']['langToggleSplitWidePages'] = '広いページを分ける';
-$localisedStrings['ja']['langTogglePageJump'] = 'なんでもページにジャンプ';
-
 /* Extra features description */
 $localisedStrings['en']['langFeaturesFirstColumn'] = 'iComics has been designed from the ground up with speed as the absolutely top priorty. Comics import quickly, they\'re silky smooth 
 														to read, and are easily configurable. And performance is the same no matter which iOS device you\'re on!';
 $localisedStrings['en']['langFeaturesSecondColumn'] = 'Not only that, iComics puts a great amount of emphasis on making managing comic files on your iOS device as flexible as possible. 
 														You can re-order comics, change their reading state, rename them and many more features are on the way!';
-
-$localisedStrings['ja']['langFeaturesFirstColumn'] = 'iComicsは最初から、スピードが最優先のようにデザインされてきました。コミックはとても早くに取り込んだり、空いたり、そして、設定を変更したりできます。そしてどんなデバイスでも、とても軽く感じます。';
-$localisedStrings['ja']['langFeaturesSecondColumn'] = 'それだけじゃなく、iComicsはコミックのファイルの管理にもとても大切にしています。簡単に並べ替えを変えたり、状態を変更したり、そして名前変更したりできます。そして、これからも、もっと機能がきます！';
-
 /* Upcoming features */
-$localisedStrings['en']['langUpcomingFeatures'] = 'Version 1.0 set the stage, and version 1.0.6 raised the bar. But it\'s not over yet.<br/>Check out these upcoming features that are right around the corner!';
+$localisedStrings['en']['langUpcomingFeatures'] = 'Version 1.0.6 helped raise the level, and version 1.1 has gone above and beyond even that. But it\'s not over yet!<br/>Check out these upcoming features coming in the future!';
 $localisedStrings['en']['langUserVoiceLink'] = 'Got an idea to make iComics even better? Suggest it! &raquo;';
 
-$localisedStrings['ja']['langUpcomingFeatures'] = 'バージョン1.0は爆発で始まりました。そして最新のバージョン1.0.6でレベルがもっと上がりました。<br/>これからの機能の予定をご覧ください！';
-$localisedStrings['ja']['langUserVoiceLink'] = 'iComicsをもっとかっこよくする考えがありますか？是非提案してください！ &raquo;';
-
 /* Footer */
-$localisedStrings['en']['langDeveloperProfile'] = 'iComics is the caffeinated machination of Tim Oliver, a 26 year old geek from Perth, Australia. When he\'s not cranking out apps, he can be found professionally developing websites, or unprofessionally singing karaoke in Japan.';
+$localisedStrings['en']['langDeveloperProfile'] = 'iComics is the caffeinated machination of Tim Oliver, a 28 year old geek from Perth, Australia.<br/> When he\'s not cranking out apps, he can be found professionally developing apps for corporate clients, or unprofessionally singing karaoke in Japan.';
 $localisedStrings['en']['langContactName'] = 'Name';
 $localisedStrings['en']['langContactEmail'] = 'Email';
 $localisedStrings['en']['langContactMessage'] = 'Message';
 $localisedStrings['en']['langContactSubmit'] = 'Submit';
-
-$localisedStrings['ja']['langDeveloperProfile'] = 'iComicsは２６歳の西オーストラリア人のティムオリバーのコーヒーで供給された作品です。ティムはアプリを作っていない時、仕事でサイトを開発したり、専門外のようにカラオケを歌ったりしています。';
-$localisedStrings['ja']['langContactName'] = '名前';
-$localisedStrings['ja']['langContactEmail'] = 'メール';
-$localisedStrings['ja']['langContactMessage'] = 'メッセージ';
-$localisedStrings['ja']['langContactSubmit'] = '送信';
--- templates/main.php
+++ templates/main.php
@@ -6,9 +6,21 @@
     <meta name="keywords" content="<?php echo $langPageKeywords; ?>" />
 	<meta name="google-site-verification" content="u3JPeQFi0Xm-S2mXiPIsgOhEKAUDaJnZU0IYoLZj32M" />
 
-    <link rel="image_src" href="/screenshot.png" />
-    <link rel="shortcut icon" href="/favicon.ico">
     
+    
+    <link rel="shortcut icon" href="/favicon/favicon.ico" type="image/x-icon" />
+	<!-- Apple Touch Icons -->
+	<link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-touch-icon-152x152.png" />
+    
+    <link rel="image_src" href="/screenshot.png" />
     <meta property="og:image" content="/screenshot.png" />
     <meta content="width=1024" name="viewport" />
 
@@ -28,51 +40,30 @@
                 avatar_size: 64,
                 count: 2,
                 loading_text: "loading tweets...",
-                template: "{text}{avatar}{user}{time} "
+                template: "{avatar}{user}{time}{text}"
             });
           });
     </script>
 
-    <script type="text/javascript">
-      var _tsq = _tsq || [];
-      _tsq.push(["setAccountName", "icomics"]);
-      _tsq.push(["fireHit", "icomics_home", []]);
-
-      (function() {
-          function z(){
-              var s = document.createElement("script");
-              s.type = "text/javascript";
-              s.async = "async";
-              s.src = window.location.protocol + "//cdn.tapstream.com/static/js/tapstream.js";
-              var x = document.getElementsByTagName("script")[0];
-              x.parentNode.insertBefore(s, x);
-          }
-          if (window.attachEvent)
-              window.attachEvent("onload", z);
-          else
-              window.addEventListener("load", z, false);
-      })();
-    </script>
 
     <link href="css/main.css?time=<?php date(); ?>" rel="stylesheet" />
   </head>
   <body>
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1&appId=396331313772228";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
     <header>
       <div class="container">
         <img src="img/iComicsHeaderArt.png" id="splash-art" width="629" height="729" alt="iComics on iPhone 6 Plus and iPad Air 2" />
 
         <img src="img/iComicsLogo.png" id="logo" width="341" height="96" alt="iComics Logo" />
-        <a href="<?php if ($langCode=='ja'):?>/jp<?php endif; ?>/app">
+     
+        <a href="/app">
           <img src="img/AppBadge.png" id="app-store-badge" width="244" height="76" alt="Now available on the App Store. (Compatible with iPhone, iPod touch and iPad. Requires iOS 7.0 or above)" />
         </a>
+
+        <ul id="app-social-icons">
+        	<li><a href="http://twitter.com/iComicsApp" target="_blank"><img src="/img/Twitter-LightGrey.png" alt="Twitter" width="31" height="25" /></a></li>
+        	<li><a href="http://instagram.com/iComicsApp" target="_blank"><img src="/img/Instagram-LightGrey.png" alt="Instagram" width="27" height="27" /></a></li>
+        	<li><a href="http://facebook.com/iComicsApp" target="_blank"><img src="/img/Facebook-LightGrey.png" alt="Facebook" width="28" height="28" /></a></li>
+        </ul>
       </div>
     </header>
     <div id="content">
@@ -140,10 +131,10 @@
           <div class="shadow-top">&nbsp;</div>
           <div class="shadow-bottom">&nbsp;</div>
           
-          <img src="img/iPad1stGen.png" alt="iComics on the original iPad" width="499" height="357" id="ipad" />
+          <img src="img/iPodTouch5thGen.png" alt="iComics on iPod touch (5th Generation)" width="386" height="398" id="ipod" />
 
           <div class="text-block">
-            <img src="img/Features1Title.png" alt="Fast, flexible and easy." width="384" height="43" class="info-title" /><br/>
+            <img src="img/Features1Title.png" alt="Fast, flexible and easy." width="361" height="41" class="info-title" /><br/>
 
             <div class="features-column">
               <?php echo $langFeaturesFirstColumn; ?>
@@ -156,27 +147,27 @@
         </div>
          <div class="info-block">
           <div id="upcoming-features">
-            <img src="img/UpcomingFeatures.png" alt="Upcoming features" width="348" height="44" class="info-title" /><br/>
+            <img src="img/UpcomingFeatures.png" alt="Upcoming features" width="333" height="42" class="info-title" /><br/>
 
             <?php echo $langUpcomingFeatures; ?>
             
             <ul class="upcoming-features-list">
-              <li>iOS 7 Support</li>
-              <li>Adding bookmarks</li>
-              <li>Separate comic collections</li>
-              <li>Comic page thumbnail index</li>
-              <li>Integrated Dropbox&reg; Importing</li>
-              <li>Integrated WiFi/WebDAV</li>
-              <li>Integrated Web browsing</li>
-              <li>Hot-jumping between comics</li>
-              <li>Extract nested archives</li>
-              <li>Crop/format comic covers</li>
-              <li>Vertical Scrolling</li>
-              <li>Dual-page spread</li>
+				<li>Free-to-download integration</li>
+				<li>Adding bookmarks</li>
+				<li>Comic page thumbnail index</li>
+				<li>Integrated Dropbox&reg; Importing</li>
+				<li>iCloud Drive support</li>
+				<li>Private comics</li>
+				<li>Integrated WiFi/WebDAV</li>
+				<li>Integrated Web browsing</li>
+				<li>Extract nested archives</li>
+				<li>Crop/format comic covers</li>
+				<li>Vertical scrolling</li>
+				<li>Dual-page spread</li>
             </ul>
             <div style="clear:both"></div>
 
-            <a href="http://bugs.icomics.co"><?php echo $langUserVoiceLink; ?></a><br/>
+            <!-- <a href="http://bugs.icomics.co"><?php echo $langUserVoiceLink; ?></a><br/> -->
 
           </div>
         </div>
@@ -186,27 +177,26 @@
       <div class="container">
         <ul id="footer-items">
           <li>
-              <img src="img/CreditsTitle.png" class="title" alt="Who made iComics?" width="251" height="26" /><br/>
+              <img src="img/CreditsTitle.png" class="title" alt="Who made iComics?" width="242" height="26" /><br/>
 
               <div class="credits-text">
                 <?php echo $langDeveloperProfile; ?>
                 <br/>
-                <div class="social-icons">
-                  <a href="http://timoliver.com.au" target="_blank"><img src="img/WebsiteIcon.png" alt="Website" width="24" height="24" /></a>
-                  <a href="http://twitter.com/TimOliverAU" target="_blank"><img src="img/TwitterIcon.png" alt="Twitter" width="24" height="24" /></a>
-                  <a href="http://facebook.com/timoliver.au" target="_blank"><img src="img/FacebookIcon.png" alt="Facebook" width="24" height="24" /></a>
-                  <a href="https://plus.google.com/114425396945647910586" target="_blank"><img src="img/GooglePlusIcon.png" alt="Google+" width="24" height="24" /></a>
-              </div>
+                <div id="tim-social-icons">
+                  <a href="http://twitter.com/TimOliverAU" target="_blank"><img src="img/Twitter-DarkGrey.png" alt="Twitter" width="25" height="21" /></a>
+                  <a href="http://instagram.com/timoliver" target="_blank"><img src="img/Instagram-DarkGrey.png" alt="Facebook" width="22" height="22" /></a>
+                  <a href="http://timoliver.com.au" target="_blank"><img src="img/Blog-DarkGrey.png" alt="Website" width="21" height="21" /></a>
+              	</div>
               </div>
 
               <img src="img/Tim.png" alt="Tim Oliver" width="118" height="150" />
           </li>
           <li>
-              <img src="img/SocialFeedTitle.png" class="title" alt="Word on the Tweets" width="248" height="26" /><br/>
+              <img src="img/SocialFeedTitle.png" class="title" alt="Word on the Tweets" width="240" height="26" /><br/>
               <div class="tweets"></div>
           </li>
           <li class="last">
-              <img src="img/SupportTitle.png" class="title" alt="Who made iComics?" width="229" height="31" />
+              <img src="img/SupportTitle.png" class="title" alt="Who made iComics?" width="220" height="31" />
 
               <div id="support-form">
                 <div id="loader" style="display: none;"><img src="img/AjaxLoader.gif" width="32" height="32" alt="Loading..." /></div>
@@ -228,14 +218,14 @@
         <div class="container">
           <div id="copyright-text">
             Copyright &copy; <a href="http://olivr.vg">Tim Oliver</a> 2012-<?php echo date('Y'); ?>. Hosted by <a href="http://www.ennoverse.com" target="_blank">Ennoverse</a>. 
-            Artwork: <a href="http://www.pixeden.com/psd-mock-up-templates/ipad-2-psd-vector-mockup-template" target="_blank">iPad 2</a> &bull; 
-                                <a href="http://pixeden.com/psd-mock-up-templates/3d-iphone-5-psd-vector-mockup-v2" target="_blank">iPhone 5</a> &bull; 
-                                <a href="http://www.pixeden.com/psd-mock-up-templates/iphone-4s-psd-vector-mockup-template" target="_blank">iPhone 4S</a> &bull; 
-                                <a href="http://www.pixeden.com/psd-mock-up-templates/ipad-psd-vector-mockup-template" target="_blank">iPad</a> &bull; 
-                                <a href="http://paulrobertlloyd.com/2009/06/social_media_icons/" target="_blank">Social Icons</a>
-
-            <a href="/"><img src="img/EN.png" alt="English" width="31" height="22" class="language-flag" /></a>
-            <a href="/jp"><img src="img/JP.png" alt="Japanese" width="31" height="22" class="language-flag" /></a>
+            Artwork: <a href="http://www.pixeden.com/psd-mock-up-templates/psd-ipad-air-2-vector-mockup" target="_blank">iPad Air 2</a> &bull; 
+                                <a href="http://www.pixeden.com/psd-mock-up-templates/iphone-6-plus-psd-vector-mockup" target="_blank">iPhone 6 Plus</a>  &bull; 
+                                <a href="http://www.pixeden.com/psd-mock-up-templates/3d-view-iphone-6-psd-vector-mockup">iPhone 6</a> &bull; 
+                                <a href="http://www.pixeden.com/psd-mock-up-templates/iphone-5s-psd-vector-mockup">iPhone 5s</a> &bull; 
+                                <a href="http://www.pixeden.com/psd-mock-up-templates/iphone-5c-psd-vector-mockup" target="_blank">iPhone 5c</a> &bull;
+                                <a href="https://dribbble.com/shots/1129682-iPod-touch-5G-PSD-Template" target="_blank">iPod touch (5th Gen)</a>
+            <br/>
+            'iComics' is a trademark of Timothy Oliver. Please remember to support the artists. Don't steal comics.        
           </div>
         </div>
       </div>
